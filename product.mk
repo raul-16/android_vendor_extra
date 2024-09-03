09c8a1a879ddddc5eb44f6403725ddf561bef548
Arian <arian.kulmer@web.de>
2024-09-03 16:26:30 +0200

    Include google sans font if existing

--- product.mk
+++ product.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Google Sans
+$(call inherit-product-if-exists, vendor/google_sans/product.mk)
+
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-cupid/device.mk)
 
