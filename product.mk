887aeeaebcecf90add5ba1b87bc0468cc9a36693
raul16 <cristian.spataru@gmail.com>
2025-10-14 22:01:48 +0300

    davinci: motor: Move popupcamera strings to devicesettings

--- product.mk
+++ product.mk
@@ -12,7 +12,3 @@ $(call inherit-product-if-exists, vendor/google_sans/product.mk)
 
 # Pixel Clocks
 $(call inherit-product-if-exists, vendor/pixel_clocks/product.mk)
-
-# Translations
-PRODUCT_PACKAGES += \
-    MotorTranslationsOverlay
